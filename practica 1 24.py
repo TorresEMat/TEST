68f1a86c51599819ffc18e929e2cbdab9eaeaef4
matias torres <torresmatias009@gmail.com>
2024-06-10 17:29:38 -0300

    primer commit

---
+++ practica 1 24.py
@@ -0,0 +1,99 @@
+#Ejercicio 6
+#x="Mi primer programa Python"
+#print (x)
+
+#Ejercicio 7
+#print("v\ne\nr\nt\ni\nc\na\nl")
+
+#Ejercicio 8
+#print("*****\n*   *\n*   *\n*   *\n*****")
+
+#Ejercicio 9
+#print("*\n***\n*****\n*******\n*********")
+
+#Ejercicio 10
+#Dato=input("ingrese un dato: ")
+#print (Dato)
+
+#Ejercicio 11
+#Valor= float(input("ingrese un valor: "))
+#print("******************************\n* El resultado es:",Valor,"\n******************************")
+
+#Ejercicio 13
+#capital=int(input("ingrese su capital a invertir: "))
+#meses=int(input("ingrese la cantidad de meses: "))
+#interes= (capital*6)/100*meses
+#dineroEnCuenta= capital + interes
+#print("el interes es de: ", interes, "\nY su dinero en cuenta ahora es: ", dineroEnCuenta)
+
+#Ejercicio 14
+#llamadas= int(input("ingrese el total de llamadas: "))
+#segundos= int(input("ingrese el total de segundos hablados: "))
+#importe= (llamadas*12)+(segundos*1.5)
+#print("el importe a pagar es: ",importe)
+
+#Ejercicio 15
+#sueldoBase= 150000
+#venta1= int(input("ingrese el precio de la venta: "))
+#venta2= int(input("ingrese el precio de la venta: "))
+#venta3= int(input("ingrese el precio de la venta: "))
+#totalVentas= venta1 + venta2 + venta3
+#porcentajeExtra=(totalVentas*10)/100
+#sueldoFinal= sueldoBase + porcentajeExtra
+#print("El sueldo final del vendedor sera: ", sueldoFinal)
+
+#Ejercicio 16
+#segundos= int(input("ingrese los segundos que desea calcular: "))
+#minutos= segundos / 60
+#horas= segundos / 3600
+#dias= segundos / 86400
+#print("Los minutos son: ",minutos,"\nLas horas son: ",horas,"\nLos dias son: ",dias)
+
+#Ejercicio 17
+#dinero= int(input("ingrese la cantidad de dinero deseada: "))
+#billete1000= dinero//1000
+#cuentaresto= dinero%1000
+#billete500= cuentaresto//500
+#cuentaresto= cuentaresto%500
+#billete200= cuentaresto//200
+#cuentaresto= cuentaresto%200
+#billete100= cuentaresto//100
+#cuentaresto= cuentaresto%100
+#billete50= cuentaresto//50
+#cuentaresto= cuentaresto%50
+#print("se entrega",billete1000,"de 1000" ,"se entrega",billete500,"de 500","se entrega",billete200,"de 200","se entrega",billete100,"de 100","se entrega",billete50,"billete 50")
+
+
+#Ejercicio 18
+#tiempo= int(input("ingrese los segundos: "))
+#dias= tiempo//86400
+#cuentaresto= tiempo%86400
+#horas= cuentaresto//3600
+#cuentaresto= cuentaresto%3600
+#minutos= cuentaresto//60
+#cuentaresto= cuentaresto%60
+#segundos= cuentaresto//1
+#print(dias,"dia",horas,"hora",minutos,"minuto",segundos,"segundos")
+
+#Ejercicio 19
+#x= input("ingrese un valor X:"))
+#y= input("ingrese un valor Y:"))
+#z=x
+#x=y
+#y=z
+#print("el valor de x es: ",x,"el valor de y es: ",y)
+
+#Ejercicio 20
+x= input("ingrese un valor X:")
+y= input("ingrese un valor Y:")
+z= input("ingrese un valor Z:")
+b=x
+x=y
+y=z
+z=b
+
+print("el valor de x es: ",x,"el valor de y es: ",y,"el valor de z es: ",z)
+
+
+
+
